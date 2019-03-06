64587c39507e46e9626cd4a64421eef809d28358
Kaelyn <31674478+kaelynx@users.noreply.github.com>
2019-03-05 20:41:11 -0500

    C# fun

---
+++ CaesarCipher/CaesarCipher/Cipher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CaesarCipher
+{
+    public class Cipher
+    { 
+        private const int alpha = 26;
+
+        public string Encrypt(string str, int key)
+        {
+            string encipher = string.Empty;
+            foreach(char c in str)
+            {
+                encipher += ToCipher(c, key);
+            }
+            return encipher;
+        }
+
+        public string Decrypt(string str, int key)
+        {
+            string original = Encrypt(str, (alpha - key));
+            return original;
+        }
+
+        public char ToCipher(char c, int key)
+        {
+            if (!char.IsLetter(c))
+            {
+                // keep spaces and punctuation
+                return c;
+            }
+            else
+            {
+                char temp = char.IsUpper(c) ? 'A' : 'a';
+                char result = (char)((((c + key) - temp) % alpha) + temp);
+                return result;
+            }
+        }
+    }
+}
---
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter encryption text:\n");
+            string encryptStr = Console.ReadLine();
+            Console.WriteLine("Enter encryption key:\n");
+            int enKey = Convert.ToInt32(Console.ReadLine());
+
+            Cipher c = new Cipher();
+
+            Console.WriteLine("Encrypted text: ");
+            string cipher = c.Encrypt(encryptStr, enKey);
+            Console.WriteLine(cipher);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Decrypted text: ");
+            string putBack = c.Decrypt(cipher, enKey);
+            Console.WriteLine(putBack);
+            Console.WriteLine("\n");
+        }
+    }
+}
